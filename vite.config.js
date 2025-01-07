74eb906e088e5eac8bcb604b328d8dab56fc133a
祖安军节度使 <>
2025-01-08 07:50:54 +0800

    Update deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const base =
-  process.env.NODE_ENV === "production" ? "/shaosong-timeline/" : "/";
+const getBase = () => {
+  // For GitHub Pages
+  if (process.env.GITHUB_PAGES === "true") {
+    return "/shaosong-timeline/";
+  }
+  // For Vercel, Netlify, or local development
+  return "/";
+};
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: getBase(),
   server: {
-    open: true, // Opens the browser when the dev server starts
+    open: true,
   },
   assetsInclude: ["**/*.csv"],
 });
